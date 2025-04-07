732ec130f012d8e95919bdbe3f1694eed31524c1
Douglas C. Rodrigues <correarodriguesdouglas@gmail.com>
2025-04-06 23:11:15 -0300

    Delete _redirects.py

--- _redirects.py
+++
@@ -1,8 +0,0 @@
-# Criar o arquivo _redirects com o conteúdo correto
-redirects_content = "/*    /index.html   200\n"
-
-file_path = "/mnt/data/_redirects"
-with open(file_path, "w") as file:
-    file.write(redirects_content)
-
-file_path
